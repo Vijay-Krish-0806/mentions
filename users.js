38fac836c847772c46a8d12d8afce7e93f21f820
Vijay Krishna Inampudi <134956134+Vijay-Krish-0806@users.noreply.github.com>
2025-03-25 14:21:09 +0530

    Update users.js

--- users.js
+++ users.js
@@ -1,31 +1,29 @@
 export const users = [
   {
     id: 1,
-    userImg: "nature.jpg",
+    userImg: "man.jpg",
     username: "User 1",
   },
   {
     id: 2,
-    userImg: "nature.jpg",
+    userImg: "woman.jpg",
     username: "Dummy 2",
   },
   {
     id: 3,
-    userImg: "nature.jpg",
+    userImg: "man.jpg",
     username: "User 3",
   },
   {
     id: 4,
-    userImg: "nature.jpg",
+    userImg: "woman.jpg",
     username: "Dummy 4",
   },
   {
     id: 5,
-    userImg: "nature.jpg",
+    userImg: "man.jpg",
     username: "User 5",
   },
-  {
-    id: 6,
     userImg: "nature.jpg",
     username: "Anonymous",
   },
